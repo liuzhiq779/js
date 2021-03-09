63162cc1682d634b60c68e802da59cdb22acf2bc
liuzhiq779 <1832875619@qq.com>
2021-03-09 16:25:35 +0800

    js继承的实现

---
+++ htmlPHP项目练习/js练习/BOM/jieque.js
@@ -0,0 +1,6 @@
+String.prototype.daxie = function(){
+    //首字母大写
+    //console.log(this) //this代表字符串本身
+    //截取瘦字母this.slice(0,1) 截取其他的 this.slice(1)
+    //this.clice(0,1).toUpperCase()  this.slice(1).toLowerCase()
+}
