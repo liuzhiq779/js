580f28d9fcd056b23e97f6db04c0c7ff87061c88
liuzhiq779 <1832875619@qq.com>
2021-03-16 14:28:35 +0800

    海贼王项目最新提交

--- htmlPHP项目练习/node/hzw/router.js
+++ htmlPHP项目练习/node/hzw/router.js
@@ -1,17 +1,30 @@
+//引入url
+var url = require('url');
+var countraller= require('./countraller')
 //路由模块，处理请求的
-
-const countraller = require('./countraller');
-
+//引入业务处理层模块
+//导出页面数据
 module.exports = {
+//服务请求处理saart是业务层传递过来的参数
+//接受服务对象
   start:function(server) {
+    //监听请求事件
     server.on('request',function(req,res){
-     var urls = req.url;
-     if(urls == '/') {
-        var countraller= require('./countraller')
+    //请求服务地址
+     var urls = url.parse(req.url,true);
+     //判断请求地址
+     if(urls.pathname == '/') {
+       //调用业务模块进行业务处理
+      //  var countraller= require('./countraller')
+        //传递响应对象，是传给业务层的参数res
         countraller.index(res);
+        //如果路径不是/默认请求其他资源
+     }else if(urls.pathname ==='/getone'){
+       //res.end('11')
+       countraller.getone(req,res,urls.query.id);
      }else{
-      var countraller = require('./countraller')
-      countraller.other(req,res)
+       //调用业务模块other方法
+      countraller.other(urls.pathname,res)
      }
     }) 
   }
